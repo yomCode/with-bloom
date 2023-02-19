69e10131d19107ae9d8c0d311bdc6538ccaf2cba
Abayomi Mustapha <107572498+yomCode@users.noreply.github.com>
2023-02-19 18:25:39 +0100

    Worked on Exchange section

--- src/screens/ExchangeRate/ExchangeRate.jsx
+++ src/screens/ExchangeRate/ExchangeRate.jsx
@@ -1,33 +1,61 @@
 import Input from "antd/es/input/Input";
 import React from "react";
 import Classes from "./ExchangeRate.module.css";
-
+import { CgArrowsExchange, CgArrowsExchangeAlt } from "react-icons/cg";
 
 
 function ExchangeRate() {
 
-  // const [amount, setAmount] = React.useState(0);
-  
+  const [amount, setAmount] = React.useState(0);
+  const [arrow, setArrow] = React.useState(false);
+  const coins = [
+    { name: "Bitcoin", symbol: "BTC"},
+    { name: "Ethereum", symbol: "ETH"},
+    {name:"Naira", symbol:"NGN"},
+    {name:"Dollar", symbol:"USD"},
+    {name: "Binance Coin", symbol: "BNB"},
+    {name: "Binance USD", symbol: "BUSD"},
+    {name: "Tether", symbol: "USDT"},
+    {name: "Dash", symbol: "DASH"},
+    {name: "Cardona Usd", symbol: "CUSD"},
+  ];
 
-  // const handleAmountChange = (e) => {
-  //   const amt = e.target.value;
-  //   amt >= 0 ?
-  //   setAmount(e.target.value) : setAmount(0);
-  // };
+  const handleAmountChange = (e) => {
+    const amt = e.target.value;
+    amt >= 0 ?
+    setAmount(e.target.value) : setAmount(0);
+  };
+
+
 
   return (
     <div className={Classes.container}>
       <div className={Classes.wrapper}>
-        <h1>Cryptocurrency Exchange Rate Calculator</h1>
+        <h1>Cryptocurrency Converter Calculator</h1>
         <div className={Classes.converter}>
-          <div className={Classes.input}>
-            <Input type="number" name="amount"  placeholder="Enter amount" />
-            <select name="" id="">
-              <option value="">USD</option>
-              <option value="">BUSD</option>
+            <input type="number" name="amount"  placeholder="Enter amount" onChange={handleAmountChange} value={amount} />
+            <div className={Classes.selectWrapper}>
+            <select name="" id="" className={Classes.sourceCurrency}>
+              {
+                coins.map((coin, index) => {
+                  return <option key={index} value={coin.symbol}>{coin.name}</option>
+                }
+              )}
             </select>
-
-        </div>
+            {
+              arrow ? < CgArrowsExchangeAlt className={Classes.arrow} onClick={() => setArrow(!arrow)} /> 
+              : 
+              < CgArrowsExchange className={Classes.arrow} onClick={() => setArrow(!arrow)} />
+            }
+            
+            <select name="" id="" className={Classes.targetCurrency}>
+              {
+                coins.map((coin, index) => {
+                  return <option key={index} value={coin.symbol}>{coin.name}</option>
+                }
+              )}
+            </select>
+            </div>
         </div>
       </div>
     </div>
